d124be700260d8857f21ee5e7660be4dc7b06a2c
ThomasNordlund1976 <96356419+ThomasNordlund1976@users.noreply.github.com>
2021-12-19 10:45:55 +0100

    test
    test

---
+++ ubs.ps1
@@ -0,0 +1,3 @@
+﻿foreach ($i in 1..200){
+        new-item "$env:HOMEDRIVE$env:HOMEPATH\desktop\I_should_not_use_unknown_USBs-$i.txt" -ItemType File -Force -Value "DONT BE A SECURITYRISK!`r`n"  
+} 
