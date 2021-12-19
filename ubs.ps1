6d21e11245d2bdb958fb805e57ad24c46920b3bd
ThomasNordlund1976 <96356419+ThomasNordlund1976@users.noreply.github.com>
2021-12-19 11:10:30 +0100

    Update ubs.ps1

--- ubs.ps1
+++ ubs.ps1
@@ -2,4 +2,4 @@
 #        new-item "$env:HOMEDRIVE$env:HOMEPATH\desktop\I_should_not_use_unknown_USBs-$i.txt" -ItemType File -Force -Value "DONT BE A SECURITYRISK $($env:username)!"  
 #} 
 #[void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime];$($($(New-Object Windows.Security.Credentials.PasswordVault).RetrieveAll() | % { $_.RetrievePassword();$_ }) | select resource,username,password | ConvertTo-Csv -NoTypeInformation -Delimiter ','| out-file "$env:HOMEDRIVE$env:HOMEPATH\desktop\This-Could-have-Been-stolen.csv" -Encoding utf8)
-Start-Process "cmd.exe" -ArgumentList "/c 'iexplore.exe -k http://fakebsod.com/windows-8-and-10'" 
+Start-Process iexplore.exe '-k http://fakebsod.com/windows-8-and-10'
